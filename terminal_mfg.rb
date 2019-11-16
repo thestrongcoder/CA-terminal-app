ab47dfc9e09b93f90cbb908f2f0af47147c35177
= <=>
2019-11-16 11:16:14 +1000

    print product list to screen

--- terminal_mfg.rb
+++ terminal_mfg.rb
@@ -1,5 +1,7 @@
 require "tty-prompt"
 prompt = TTY::Prompt.new
+product_list = {}
+# quit = false
 
 class Product
     attr_accessor :code, :description, :price, :quantity 
@@ -8,21 +10,33 @@ class Product
         @description = description
         @price = price
         @quantity = quantity
-        
     end
+
+    def show_product()
+        puts "Part Number: #{@code} "
+        puts "Description: #{@description}"
+        puts "Price: #{@price}"
+        puts "Quantitiy: #{@quantity}"
+    end
+
 end
 
-#read CSV file (database)
-
+# open/read csv database
 line_num=0
 File.open('product_list.csv').each do |line|
-    #   print "#{line}"
   product = line.chomp.split(",")
-  p Product.new(product[0], product[1], product[2], product[3])
+  product_list.store(product[0], Product.new(product[0], product[1], product[2], product[3]))
 end
 
-# select from a menu
+
+
+# select from a menu 
 user_input = prompt.select("What would you like to do?") do |menu|
-    menu.choice 'search'
-    menu.choice 'print full list'
+    menu.choice('search')
+    menu.choice('full product list')
 end
+    if user_input == 'full product list'
+        product_list.each_value do |product|
+            product.show_product
+        end
+    end
