1703f6b0c0df96cacfe7d1abbee6dc1688b09712
= <=>
2019-11-16 16:14:29 +1000

    show selected product details

--- terminal_mfg.rb
+++ terminal_mfg.rb
@@ -5,6 +5,7 @@ product_list = {}
 
 class Product
     attr_accessor :code, :description, :price, :quantity 
+
     def initialize(code, description, price, quantity)
         @code = code
         @description = description
@@ -13,10 +14,12 @@ class Product
     end
 
     def show_product()
+        puts " "
         puts "Part Number: #{@code}"
         puts "Description: #{@description}"
         puts "Price: #{@price}"
         puts "Quantitiy: #{@quantity}"
+        puts "____________________________________________________"
     end
 
 end
@@ -28,12 +31,11 @@ File.open('product_list.csv').each do |line|
   product_list.store(product[0], Product.new(product[0], product[1], product[2], product[3]))
 end
 
-
-
 # select from a menu 
 user_input = prompt.select("What would you like to do?") do |menu|
     menu.choice('search')
     menu.choice('full product list')
+    menu.choice('create product')
 end
 if user_input == 'full product list'
     product_list.each_value do |product|
@@ -44,9 +46,13 @@ end
 if user_input == 'search'
     search_product = %w()
     search_input = prompt.select('Search for a product', filter: true, require: true) do |item|
-        product_list.each_value do |value|
-            item.choice(value.description)
+        product_list.each_value do |product|
+            item.choice(product.description, product.code)
         end
     end
+    # show the selected product key and values
+    puts product_list[search_input].show_product()
 end
+
+
 
